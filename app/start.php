abc2f4e2b402dedcc8d44e0cd4ed8cbb8856b107
mustafa kostek <mustafakostek@gmail.com>
2018-12-23 00:35:00 +0300

    Update start.php
    app/start.php dosyasındaki define('BASE_URL','http://127.0.0.1:81/projects/basic_cms');//buradaki başlama linkini kendi rootunuza göre biçimlendirin

--- app/start.php
+++ app/start.php
@@ -3,7 +3,7 @@ ini_set('display_errors','On');
 
 define('APP_ROOT',__DIR__);
 define('VIEW_ROOT',APP_ROOT.'/views');
-define('BASE_URL','http://127.0.0.1:81/projects/basic_cms');
+define('BASE_URL','http://127.0.0.1:81/projects/basic_cms');//buradaki başlama linkini kendi rootunuza göre biçimlendirin
 //echo APP_ROOT;
 require 'functions.php';
 $db=new PDO('mysql:host=127.0.0.1;dbname=cms','root','');
